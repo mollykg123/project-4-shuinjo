d4f082e7ed4258ba5a422edacd49bc0e412a5f63
Molly Gregson <mollykg123@msn.com>
2024-05-30 07:55:31 +0100

    set up user model and item model

---
+++ items/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Item(models.Model):
+    title = models.CharField(max_length=50)
+    image = models.CharField(max_length=100)
+    description = models.CharField(max_length=200, blank=True, null=True)
+    may_contain = models.CharField(max_length=50)
+    desired_trades = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(
+        'users.User',
+        related_name = 'items_created',
+        on_delete=models.CASCADE,
+        blank=True       
+    )
+ 
+    def __str__(self):
+        return f'{self.title}'     
